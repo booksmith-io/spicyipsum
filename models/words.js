cc3c0d64b4a6b098a0667cbd7bd7ae3097df40e1
Blaine Motsinger <blaine@renderorange.com>
2026-01-11 14:14:31 -0600

    GH-8: add db results caching to the words model

--- models/words.js
+++ models/words.js
@@ -2,6 +2,9 @@
 
 const base = require("./base");
 
+const node_cache = require("node-cache");
+const cache = new node_cache();
+
 class Words extends base.Base {
     constructor() {
         super();
@@ -34,14 +37,52 @@ class Words extends base.Base {
             }
         }
 
-        this._types["rows"] = await this.dbh("types")
-            .select([
-                "type_id",
-                "name",
-            ]);
+        const cached_types_rows = cache.get("types_select_type_id_name");
+        if (cached_types_rows !== undefined) {
+            if (process.env["NODE_ENV"] === 'development') {
+                console.log("[debug] 'types_select_type_id_name' cache key was found");
+            }
+            this._types["rows"] = cached_types_rows;
+        }
+        else {
+            this._types["rows"] = await this.dbh("types")
+                .select([
+                    "type_id",
+                    "name",
+                ]);
 
-        for (const row of this._types["rows"]) {
-            this._types["obj"][row["name"]] = row["type_id"];
+            let cache_set_result = cache.set("types_select_type_id_name", this._types["rows"]);
+            if (cache_set_result !== undefined) {
+                if (process.env["NODE_ENV"] === 'development') {
+                    console.log("[debug] 'types_select_type_id_name' cache key was set");
+                }
+            }
+            else {
+                console.error(`[error] 'types_select_type_id_name' cache key failed to set`);
+            }
+        }
+
+        const cached_types_obj = cache.get("types_obj");
+        if (cached_types_obj !== undefined) {
+            if (process.env["NODE_ENV"] === 'development') {
+                console.log("[debug] 'types_obj' cache key was found");
+            }
+            this._types["obj"] = cached_types_obj;
+        }
+        else {
+            for (const row of this._types["rows"]) {
+                this._types["obj"][row["name"]] = row["type_id"];
+            }
+
+            let cache_set_result = cache.set("types_obj", this._types["obj"]);
+            if (cache_set_result !== undefined) {
+                if (process.env["NODE_ENV"] === 'development') {
+                    console.log("[debug] 'types_obj' cache key was set");
+                }
+            }
+            else {
+                console.error(`[error] 'types_obj' cache key failed to set`);
+            }
         }
 
         // just for good measure.  this shouldn't ever happen if the database is loaded.
@@ -63,9 +104,29 @@ class Words extends base.Base {
             selector["type_ids"].push(this._types["obj"]["wyrd"]);
         }
 
-        this._words = await this.dbh("words")
-            .select("text")
-            .whereIn("type_id", selector["type_ids"]);
+        const cache_key = `words_select_text_type_id_in_${selector["type_ids"].join("_")}`;
+        const cached_words = cache.get(cache_key);
+        if (cached_words !== undefined) {
+            if (process.env["NODE_ENV"] === 'development') {
+                console.log(`[debug] '${cache_key}' cache key was found`);
+            }
+            this._words = cached_words;
+        }
+        else {
+            this._words = await this.dbh("words")
+                .select("text")
+                .whereIn("type_id", selector["type_ids"]);
+
+            let cache_set_result = cache.set(cache_key, this._words);
+            if (cache_set_result !== undefined) {
+                if (process.env["NODE_ENV"] === 'development') {
+                    console.log(`[debug] '${cache_key}' cache key was set`);
+                }
+            }
+            else {
+                console.error(`[error] '${cache_key}' cache key failed to set`);
+            }
+        }
 
         // just for good measure.  this shouldn't ever happen if the database is loaded.
         if (this._words.length === 0) {
